7ee506f6f8f448c3ae1614bdc88e8bdc7a9cc5e3
Garrett Smith <ggasmithh@users.noreply.github.com>
2020-04-22 21:20:20 -0500

    A step back: first, a functional linked list

--- main.c
+++ main.c
@@ -2,71 +2,63 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-typedef struct __node_t {
+typedef struct node {
     int data;
-    struct __node_t* next;
+    struct node* next;
     pthread_mutex_t lock;
 } node_t;
 
-typedef struct __linked_list {
-    node_t* tail;
-    node_t* head;
-} linked_list_t;
-
-void* create_and_init_node() {
-    node_t* node = calloc(1, sizeof(node_t));
-    pthread_mutex_init(&node->lock, NULL);
-    return node;
-}
-
-void init_linked_list(void* l) {
-    linked_list_t* linked_list = (linked_list_t*) l;
-    node_t* tmp = (node_t*) create_and_init_node();
-    tmp->next = (node_t*) linked_list->tail;
-    linked_list->head = tmp;
-    linked_list->tail = tmp;
+node_t* create_and_init_node() {
+    node_t* n = (node_t*) calloc(1, sizeof(node_t));
+    //pthread_mutex_init(&n->lock, NULL);
+    return n;
 }
 
 // releases lock on current node, acquires lock on next node, returns pointer to next node
-void* traverse(void* n) {
-    node_t* curr_node = (node_t*) n;
-    node_t* next_node = (node_t*) &curr_node->next;
-    
-    pthread_mutex_unlock(&curr_node->lock);
-    pthread_mutex_lock(&next_node->lock);
-    
-    return next_node;
+node_t* traverse(node_t* curr_node) {
+    if (curr_node != NULL && curr_node->next != NULL) {
+        node_t* next_node = curr_node->next;
+        
+        //pthread_mutex_unlock(&curr_node->lock);
+        //pthread_mutex_lock(&next_node->lock);
+        
+        return next_node;
+    } else {
+        return NULL;
+    }
 }
 
 // returns the data for a node at a given address
-int get_node_data(void* n) {
-     node_t* node = (node_t*) n;
-    return node->data;
+int get_node_data(node_t* target_node) {
+    return target_node->data;
 }
 
 // pushes a node with the given data to the end of the given linked list and updates its tail
-void push(int data, void* l) {
-    linked_list_t* linked_list = (linked_list_t*) l;
-    node_t* tmp = (node_t*) create_and_init_node();
-    tmp->data = data;
-    linked_list->tail->next = tmp;
-    linked_list->tail = tmp;
+node_t* push(int new_data, node_t* last_node) {
+    node_t* tmp = create_and_init_node();
+    tmp->data = new_data;
+    tmp->next = NULL;
+    last_node->next = tmp;
+    return tmp;
 }
 
 int main() {
-    linked_list_t* linked_list;
-    node_t* current_node;
+    node_t* head = create_and_init_node();
 
-    init_linked_list(linked_list);
-    current_node = linked_list->head;
+    // Used for traversal purposes. Points to the node that the program is currently "looking at"
+    node_t* current_node = head;
 
     for (int i = 0; i < 10; i++) {
-        push(i, linked_list);
+        current_node = push(i, current_node);
     }
+
+    current_node = head;
 
     for (int i = 0; i < 10; i++) {
         current_node = traverse(current_node);
-        printf("got number %d", get_node_data(current_node));
+        if (current_node != NULL) {
+            printf("got number %d\n", get_node_data(current_node));
+        }
     }
 
     return 0;
