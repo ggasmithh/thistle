faaa286d4cb62c847d0025cedfdcd0cd6147fe0e
Garrett Smith <ggasmithh@users.noreply.github.com>
2020-04-24 15:48:50 -0500

    Started single_lock.c, removed header file

--- hand_over_hand.c
+++ hand_over_hand.c
@@ -1,4 +1,27 @@
-#include "thistle.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+
+//#define LIMIT (1000000)
+#define LIMIT (10)
+
+typedef struct node {
+    int data;
+    struct node* next;
+    pthread_mutex_t lock;
+} node_t;
+
+typedef struct counter {
+    int volatile value;
+} counter_t;
+
+typedef struct thread_args {
+    node_t* node;
+    counter_t* counter;
+    int limit;
+} thread_args_t;
+
 
 // only using non-stdint.h types in this b/c its required in the spec lol
 
